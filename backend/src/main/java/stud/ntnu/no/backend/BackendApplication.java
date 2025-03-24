3138f6c17edf9a1ff0b3f94031e5e8eba81d4854
Kaamya Shinde <kaamyashinde@gmail.com>
2025-03-24 04:49:10 +0100

    add comment to test heroku backend deployment

--- backend/src/main/java/stud/ntnu/no/backend/BackendApplication.java
+++ backend/src/main/java/stud/ntnu/no/backend/BackendApplication.java
@@ -2,7 +2,7 @@ package stud.ntnu.no.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//this is a comment to check if herokue rebuilds again properly and deploys correctly :)
 @SpringBootApplication
 public class BackendApplication {
 
