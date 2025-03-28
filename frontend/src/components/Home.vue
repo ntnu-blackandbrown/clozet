d8ff235b0c3e7b0e66fb420ab036735124f343a7
Kaamya Shinde <kaamyashinde@gmail.com>
2025-03-28 05:48:50 +0100

    add template contents to main page

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,15 +1,31 @@
 <script setup>
 import ProductList from './ProductList.vue'
+import Badge from './Badge.vue'
 </script>
 
 <template>
-  <div class="home">
+  <h1>Welcome to Clozet!</h1>
+  <h3>The new way to shop for clothes</h3>
+
+  <!--get the five categories with the most products from the database -->
+  <div class="badge-container">
+    <Badge type="category" name="Tops" />
+    <Badge type="category" name="Bottoms" />
+    <Badge type="category" name="Dresses" />
+    <Badge type="category" name="Accessories" />
+    <Badge type="category" name="Shoes" />
+  </div>
+  <div>
+    <input type="text" placeholder="Search for a product" />
+  </div>
+
+  <div class="featured-products">
     <ProductList />
   </div>
 </template>
 
 <style scoped>
-.home {
+.featured-products {
   width: 100%;
 }
 </style>
