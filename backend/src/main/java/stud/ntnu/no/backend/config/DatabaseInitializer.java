f044521ed5aa442bab0057ca7a19538e8b5c4dac
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-03-29 07:44:54 +0100

    Database test and prod sepratation

--- backend/src/main/java/stud/ntnu/no/backend/config/DatabaseInitializer.java
+++ backend/src/main/java/stud/ntnu/no/backend/config/DatabaseInitializer.java
@@ -3,17 +3,19 @@ package stud.ntnu.no.backend.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import stud.ntnu.no.backend.user.entity.User;
 import stud.ntnu.no.backend.user.repository.UserRepository;
 
 @Configuration
+@Profile("!test") // Denne initializeren kjøres ikke når testprofilen er aktiv
 public class DatabaseInitializer {
 
   @Bean
   CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
     return args -> {
-      // Slett alle eksisterende brukere (og testdata) før vi setter inn nye data
+      // Slett alle eksisterende brukere (inkludert testdata) før vi setter inn admin-brukeren
       userRepository.deleteAll();
 
       // Legg inn admin-brukeren på nytt
