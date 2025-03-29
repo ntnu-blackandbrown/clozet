31cd979847ba5cfbcc222d0d8e9815f6e87fa655
Kaamya Shinde <kaamyashinde@gmail.com>
2025-03-29 11:24:10 +0100

    add messages view to navbar

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -14,6 +14,11 @@ const router = createRouter({
       component: () => import('@/views/CreateProductView.vue'),
     },
     {
+      path: '/messages',
+      name: 'messages',
+      component: () => import('@/views/MessagesView.vue'),
+    },
+    {
       path: '/profile',
       name: 'profile',
       component: () => import('@/views/UserProfileView.vue'),
