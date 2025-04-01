822a6ef1fec3a6db97519a7c1a755139d93b3429
Kaamya Shinde <kaamyashinde@gmail.com>
2025-04-01 14:56:08 +0200

    rename packages with small letters

--- backend/src/test/java/stud/ntnu/no/backend/ShippingOption/Controller/ShippingOptionControllerTest.java
+++ backend/src/test/java/stud/ntnu/no/backend/ShippingOption/Controller/ShippingOptionControllerTest.java
@@ -1,4 +1,4 @@
-package stud.ntnu.no.backend.ShippingOption.controller;
+package stud.ntnu.no.backend.shippingoption.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
