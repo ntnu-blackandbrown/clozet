da2e1ab61c09146643b69d146d6806608f086aec
Kaamya Shinde <kaamyashinde@gmail.com>
2025-04-01 15:47:28 +0200

    refactor package name in test to correct

--- backend/src/test/java/stud/ntnu/no/backend/shippingoption/controller/ShippingOptionControllerTest.java
+++ backend/src/test/java/stud/ntnu/no/backend/shippingoption/controller/ShippingOptionControllerTest.java
@@ -1,4 +1,4 @@
-package stud.ntnu.no.backend.shippingoptions.controllers;
+package stud.ntnu.no.backend.shippingoption.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
