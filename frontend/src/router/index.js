6cc77e01bd152ff1ffeb9ec46dab8a595e2d01b2
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-04-02 08:10:27 +0200

    cleaning of wrong components in frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import EmailVerification from '@/views/EmailVerification.vue'
-import Dashboard from '@/views/Dashboard.vue'
-import ForgotPasswordView from '@/views/ForgotPasswordView.vue'
-import ResetPasswordView from '@/views/ResetPasswordView.vue'
-import UserProfileView from '@/views/UserProfileView.vue'
 import LoginRegisterView from '@/views/LoginRegisterView.vue'
-import VerifyInfo from '@/views/VerifyInfo.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,41 +11,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/verify',
-      name: 'verify',
-      component: EmailVerification
-    },
-    {
-      path: '/verify-info',
-      name: 'verify-info',
-      component: VerifyInfo
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: Dashboard,
-      meta: { requiresAuth: true }
-    },
-    {
       path: '/login',
       name: 'login',
       component: LoginRegisterView
-    },
-    {
-      path: '/forgot-password',
-      name: 'forgot-password',
-      component: ForgotPasswordView
-    },
-    {
-      path: '/reset-password',
-      name: 'reset-password',
-      component: ResetPasswordView
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: UserProfileView,
-      meta: { requiresAuth: true }
     }
   ]
 })
--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -1,15 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginRegisterView from '@/views/LoginRegisterView.vue'
-import VerifyInfo from '@/views/VerifyInfo.vue'
-import EmailVerification from '@/views/EmailVerification.vue'
-import Dashboard from '@/views/Dashboard.vue'
+
 
 const routes = [
   { path: '/', name: 'Home', component: LoginRegisterView },
   { path: '/login', name: 'Login', component: LoginRegisterView },
-  { path: '/verify-info', name: 'VerifyInfo', component: VerifyInfo },
-  { path: '/verify', name: 'EmailVerification', component: EmailVerification },
-  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
 ]
 
 const router = createRouter({
