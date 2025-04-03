5413c3d2873fb0c5d919eac51f6bffdca1ef2e46
Kaamya Shinde <kaamyashinde@gmail.com>
2025-04-03 15:26:38 +0200

    remve comments in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,11 +4,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router/router'
-//import LoginRegisterModal from './components/LoginRegisterModal.vue'
-//import ProductDisplay from './components/ProductDisplay.vue'
+
 const app = createApp(App)
-//const app = createApp(LoginRegisterModal)
-//const app = createApp(ProductDisplay)
 app.use(createPinia())
 app.use(router)
 
