7510057f8adc897b7f7dfb82938044f73d2bfd72
Kaamya Shinde <kaamyashinde@gmail.com>
2025-04-04 17:43:00 +0200

    redirect user to main when logged out + update its test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue'
 import LoginRegisterModal from '@/views/LoginRegisterView.vue'
-import { RouterView } from 'vue-router'
+import { RouterView, useRouter } from 'vue-router'
 import { useAuthStore } from './stores/AuthStore'
 
+const router = useRouter()
 const authStore = useAuthStore()
 const showLoginModal = ref(false)
 const statusMessage = ref('')
@@ -20,6 +21,7 @@ onMounted(async () => {
 
 async function logout() {
   await authStore.logout()
+  router.push('/')
 }
 </script>
 
