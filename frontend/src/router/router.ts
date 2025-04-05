c21194b949bb36ab9ebc758b948acf0bbbc8773f
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-04-05 21:07:42 +0200

    add token verifier

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -11,6 +11,11 @@ const router = createRouter({
       component: () => import('@/views/HomeView.vue'),
     },
     {
+      path: '/api/auth/verify',
+      name: 'toker-verifier',
+      component: () => import('@/views/TokerVerifier.vue'),
+    },
+    {
       path: '/products/:id',
       name: 'product-detail',
       component: () => import('@/views/HomeView.vue'),
