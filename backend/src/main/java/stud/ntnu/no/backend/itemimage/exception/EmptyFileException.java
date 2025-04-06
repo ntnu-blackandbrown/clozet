7d013e669b7ccc7c20e42de72270a15e64083e61
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-04-06 20:36:51 +0200

    inconsistencies fix

--- backend/src/main/java/stud/ntnu/no/backend/itemimage/exception/EmptyFileException.java
+++ backend/src/main/java/stud/ntnu/no/backend/itemimage/exception/EmptyFileException.java
@@ -1,9 +1,11 @@
 package stud.ntnu.no.backend.itemimage.exception;
 
+import stud.ntnu.no.backend.common.exception.BaseException;
+
 /**
  * Exception thrown when an attempt is made to process an empty file.
  */
-public class EmptyFileException extends RuntimeException {
+public class EmptyFileException extends BaseException {
     /**
      * Constructs a new EmptyFileException with the specified detail message.
      *
--- backend/src/main/java/stud/ntnu/no/backend/itemimage/exception/InvalidFileTypeException.java
+++ backend/src/main/java/stud/ntnu/no/backend/itemimage/exception/InvalidFileTypeException.java
@@ -1,9 +1,11 @@
 package stud.ntnu.no.backend.itemimage.exception;
 
+import stud.ntnu.no.backend.common.exception.BaseException;
+
 /**
  * Exception thrown when a file type is not supported.
  */
-public class InvalidFileTypeException extends RuntimeException {
+public class InvalidFileTypeException extends BaseException {
     /**
      * Constructs a new InvalidFileTypeException with the specified detail message.
      *
