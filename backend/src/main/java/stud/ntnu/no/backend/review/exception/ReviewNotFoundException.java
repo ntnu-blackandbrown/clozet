4b023548305e8ac9449296c8b0ec262e2dd4eb30
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-04-06 21:33:35 +0200

    review logging implementation

--- backend/src/main/java/stud/ntnu/no/backend/review/exception/ReviewNotFoundException.java
+++ backend/src/main/java/stud/ntnu/no/backend/review/exception/ReviewNotFoundException.java
@@ -8,14 +8,6 @@ import stud.ntnu.no.backend.common.exception.BaseException;
  * This exception is mapped to a 404 Not Found HTTP status code.
  */
 public class ReviewNotFoundException extends BaseException {
-    /**
-     * Constructs a new ReviewNotFoundException with the specified review ID.
-     *
-     * @param id the ID of the review that was not found
-     */
-    public ReviewNotFoundException(Long id) {
-        super("Could not find review with id: " + id);
-    }
 
     /**
      * Constructs a new ReviewNotFoundException with the specified detail message.
--- backend/src/main/java/stud/ntnu/no/backend/review/exception/ReviewValidationException.java
+++ backend/src/main/java/stud/ntnu/no/backend/review/exception/ReviewValidationException.java
@@ -6,7 +6,6 @@ import stud.ntnu.no.backend.common.exception.BaseException;
  * <p>
  * This exception is mapped to a 400 Bad Request HTTP status code.
  */
-//@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ReviewValidationException extends BaseException {
     /**
      * Constructs a new ReviewValidationException with the specified detail message.
