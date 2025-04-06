2cc140e75a02495bf6ea0460f7634031bb898bc1
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-04-06 15:16:27 +0200

    location javadoc rest of package

--- backend/src/main/java/stud/ntnu/no/backend/location/exception/LocationNotFoundException.java
+++ backend/src/main/java/stud/ntnu/no/backend/location/exception/LocationNotFoundException.java
@@ -3,8 +3,18 @@ package stud.ntnu.no.backend.location.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when a location is not found.
+ * <p>
+ * This exception is mapped to a 404 Not Found HTTP status code.
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class LocationNotFoundException extends RuntimeException {
+    /**
+     * Constructs a new LocationNotFoundException with the specified location ID.
+     *
+     * @param id the ID of the location that was not found
+     */
     public LocationNotFoundException(Long id) {
         super("Could not find location with id: " + id);
     }
--- backend/src/main/java/stud/ntnu/no/backend/location/exception/LocationValidationException.java
+++ backend/src/main/java/stud/ntnu/no/backend/location/exception/LocationValidationException.java
@@ -3,8 +3,18 @@ package stud.ntnu.no.backend.location.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when a location validation fails.
+ * <p>
+ * This exception is mapped to a 400 Bad Request HTTP status code.
+ */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class LocationValidationException extends RuntimeException {
+    /**
+     * Constructs a new LocationValidationException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public LocationValidationException(String message) {
         super(message);
     }
