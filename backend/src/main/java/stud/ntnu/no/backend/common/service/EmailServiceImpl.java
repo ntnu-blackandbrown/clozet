24c9460029b9b5d3118f4e4506e38b3c5584984e
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-04-08 11:40:08 +0200

    emial styling

--- backend/src/main/java/stud/ntnu/no/backend/common/service/EmailServiceImpl.java
+++ backend/src/main/java/stud/ntnu/no/backend/common/service/EmailServiceImpl.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
