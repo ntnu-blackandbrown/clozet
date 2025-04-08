f77e1c1ff49cff20d82b0188b7500a1292a4c66c
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-04-08 17:09:31 +0200

    revamped database structure

--- backend/src/main/resources/db/migration/V1__update_favorite_active_field.sql
+++
@@ -1,5 +0,0 @@
--- Add a default value constraint to the active column
-ALTER TABLE favorites ALTER COLUMN active SET DEFAULT false;
-
--- Update any existing rows where active might be null
-UPDATE favorites SET active = false WHERE active IS NULL; 
